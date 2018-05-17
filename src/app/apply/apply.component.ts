2546bc13a2111d73d72be4a6ee9eda1af8159ac4
deepak-ranolia <d.ranolia92@gmail.com>
2018-05-17 14:48:09 +0530

    sunday between two days working

--- src/app/apply/apply.component.ts
+++ src/app/apply/apply.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl } from '@angular/forms'
 import { LmsService } from '../lms.service'
-import { MatDatepickerModule } from '@angular/material/datepicker'
-
-
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 import * as moment from 'moment'
 
@@ -14,39 +12,121 @@ import * as moment from 'moment'
 })
 
 export class ApplyComponent implements OnInit {
-  selectedValue: string
+  
+  selectedValue : string
   showUs = false
   loader : boolean = false
-
-  leaves = [
-    { value: 'bal_cl', tol: 'Casual Leave', bal:'10' },
-    { value: 'bal_sl', tol: 'Sick Leave', bal:'10' },
-    { value: 'bal_pl', tol: 'Privileged Leave', bal:'10' },
-    { value: 'bal_eol', tol: 'Extra Ordinary Leave', bal:'10' },
-    { value: 'bal_ptl', tol: 'Materinity Leave', bal:'10' },
-    { value: 'bal_mtl', tol: 'Paterinity Leave', bal:'10' }
-  ]
-
-  materialDate = new FormControl(new Date())
-  serializedDate = new FormControl((new Date()).toISOString())
   
+  firstDate : any
+  secondDate : any // string[] = []
+  // result : any
+  date : any
+  month : any
+  year : any
+
+  // leaves = [
+  //   { value: 'bal_cl', tol: 'Casual Leave', bal:'10' },
+  //   { value: 'bal_sl', tol: 'Sick Leave', bal:'10' },
+  //   { value: 'bal_pl', tol: 'Privileged Leave', bal:'10' },
+  //   { value: 'bal_eol', tol: 'Extra Ordinary Leave', bal:'10' },
+  //   { value: 'bal_ptl', tol: 'Materinity Leave', bal:'10' },
+  //   { value: 'bal_mtl', tol: 'Paterinity Leave', bal:'10' }
+  // ]
   constructor(private lms:LmsService){
     this.lms.emitsload.subscribe( el => this.loader = el )
     this.lms.showLoader()
-
-    var start = moment('2018-05-14'), // first date
-    end   = moment('2018-06-02'), // second date
-    day   = 0;                    // Sunday
-
-    var result = [];
-    var current = start.clone();
-
-    while (current.day(7 + day).isBefore(end)) {
-      result.push(current.clone());
-    }
-    console.log(result.map(m => m.format('LLLL')));
   }
 
   ngOnInit(){}
-  
+
+  firstDateEvent( event: MatDatepickerInputEvent<Date>) {
+    // get date
+    let date = event.value.getDate()
+    let d = date
+    if ( d < 10 ){
+      this.date = '0' + d
+      // this.date
+    } else this.date = d
+    // now get month
+    let month = event.value.getMonth()
+    let m = month
+    if ( m < 10 ) {
+      m++
+      this.month = '0' + m
+      this.month
+    } else {
+      m++
+      this.month = m
+    }
+    // now get year 
+    let year = event.value.getFullYear()
+    this.year = year
+    
+    // get fulldate
+    let fulldate = String( this.year+'-'+this.month+'-'+this.date ) 
+    this.firstDate = fulldate
+  }
+
+  secondDateEvent( event: MatDatepickerInputEvent<Date> ) {
+    // get date
+    let date = event.value.getDate()
+    let d = date
+    if ( d < 10 ){
+      this.date = '0' + d
+      this.date
+    } else this.date = d
+    
+    // now get month
+    let month = event.value.getMonth()
+    let m = month
+    if ( m < 10 ) {
+      m++
+      this.month = '0' + m
+      this.month
+    } else {
+      m++
+      this.month = m
+    }
+    // now get year 
+    let year = event.value.getFullYear()
+    this.year = year
+    
+    // get fulldate
+    let fulldate = String( this.year+'-'+this.month+'-'+this.date ) 
+    this.secondDate = fulldate
+    
+    // Calculate sundays between two days using Moment JS
+    var f = moment(this.firstDate),
+    s = moment(this.secondDate),
+    sunday = 0 // Sunday
+
+    let result = []
+    var current = f.clone()
+
+    while ( current.day(7 + sunday).isBefore(s) ) {
+      result.push( current.clone() )
+      }
+    console.log(result.map( m => m ).length )
+
+
+    // if ( frstmonth  > scndmonth ){
+    //   //
+    // } else {
+    //   //
+    // }
+
+
+    // if ( frstDate  > scndDate ){
+    //   //
+    // } else {
+    //   //
+    // }
+    
+  }
+
+  disableSunDay = ( d: Date ) : boolean => {
+    const day = d.getDay()
+    return day !== 0 // && day !== 6 // Uncomment if saturday is disabled too
+  }
+
 }
