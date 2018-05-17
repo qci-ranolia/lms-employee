f09a073eca686cf8aa55fc6d714c0c0136b7360f
deepak-ranolia <d.ranolia92@gmail.com>
2018-05-17 17:41:44 +0530

    sunday disabled

--- src/app/apply/apply.component.ts
+++ src/app/apply/apply.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl } from '@angular/forms'
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { LmsService } from '../lms.service'
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
-
 import * as moment from 'moment'
 
 @Component({
@@ -12,59 +11,76 @@ import * as moment from 'moment'
 })
 
 export class ApplyComponent implements OnInit {
+  isLinear = true
+  firstFormGroup: FormGroup
+  secondFormGroup: FormGroup
   
   selectedValue : string
   showUs = false
   loader : boolean = false
-  
+  isFirstDateSelected : boolean = true
+
   firstDate : any
   secondDate : any // string[] = []
   // result : any
+
   date : any
   month : any
   year : any
 
-  // leaves = [
-  //   { value: 'bal_cl', tol: 'Casual Leave', bal:'10' },
-  //   { value: 'bal_sl', tol: 'Sick Leave', bal:'10' },
-  //   { value: 'bal_pl', tol: 'Privileged Leave', bal:'10' },
-  //   { value: 'bal_eol', tol: 'Extra Ordinary Leave', bal:'10' },
-  //   { value: 'bal_ptl', tol: 'Materinity Leave', bal:'10' },
-  //   { value: 'bal_mtl', tol: 'Paterinity Leave', bal:'10' }
-  // ]
-  constructor(private lms:LmsService){
+  leavedays : any  
+  leaves = [
+    { value: 'bal_cl', tol: 'Casual Leave', bal:'10' },
+    { value: 'bal_sl', tol: 'Sick Leave', bal:'10' },
+    { value: 'bal_pl', tol: 'Privileged Leave', bal:'10' },
+    { value: 'bal_eol', tol: 'Extra Ordinary Leave', bal:'10' },
+    { value: 'bal_ptl', tol: 'Materinity Leave', bal:'10' },
+    { value: 'bal_mtl', tol: 'Paterinity Leave', bal:'10' }
+  ]
+
+  minDate = new Date()
+  constructor( private lms:LmsService, private _formBuilder: FormBuilder){
     this.lms.emitsload.subscribe( el => this.loader = el )
     this.lms.showLoader()
   }
 
-  ngOnInit(){}
+  ngOnInit(){
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
 
-  firstDateEvent( event: MatDatepickerInputEvent<Date>) {
+  firstDateEvent( event: MatDatepickerInputEvent<Date>){
     // get date
     let date = event.value.getDate()
     let d = date
     if ( d < 10 ){
       this.date = '0' + d
-      // this.date
     } else this.date = d
-    // now get month
+
+    // Now get month
     let month = event.value.getMonth()
     let m = month
     if ( m < 10 ) {
       m++
       this.month = '0' + m
-      this.month
     } else {
       m++
       this.month = m
     }
-    // now get year 
+    
+    // Now get year 
     let year = event.value.getFullYear()
     this.year = year
     
-    // get fulldate
+    // Get fulldate
     let fulldate = String( this.year+'-'+this.month+'-'+this.date ) 
     this.firstDate = fulldate
+    this.isFirstDateSelected = false
+    this.leavedays = 1
   }
 
   secondDateEvent( event: MatDatepickerInputEvent<Date> ) {
@@ -73,7 +89,6 @@ export class ApplyComponent implements OnInit {
     let d = date
     if ( d < 10 ){
       this.date = '0' + d
-      this.date
     } else this.date = d
     
     // now get month
@@ -82,11 +97,11 @@ export class ApplyComponent implements OnInit {
     if ( m < 10 ) {
       m++
       this.month = '0' + m
-      this.month
     } else {
       m++
       this.month = m
     }
+    
     // now get year 
     let year = event.value.getFullYear()
     this.year = year
@@ -95,36 +110,21 @@ export class ApplyComponent implements OnInit {
     let fulldate = String( this.year+'-'+this.month+'-'+this.date ) 
     this.secondDate = fulldate
     
-    // Calculate sundays between two days using Moment JS
+    //Calculate sundays between two days using Moment JS
     var f = moment(this.firstDate),
     s = moment(this.secondDate),
     sunday = 0 // Sunday
-
     let result = []
     var current = f.clone()
-
     while ( current.day(7 + sunday).isBefore(s) ) {
       result.push( current.clone() )
-      }
-    console.log(result.map( m => m ).length )
-
-
-    // if ( frstmonth  > scndmonth ){
-    //   //
-    // } else {
-    //   //
-    // }
-
-
-    // if ( frstDate  > scndDate ){
-    //   //
-    // } else {
-    //   //
-    // }
-    
+    }
+    // calculate leavedays
+    var totalDays = s.diff(f, 'days')
+    this.leavedays =  1 + totalDays - result.map( m => m ).length  
   }
-
-  disableSunDay = ( d: Date ) : boolean => {
+  
+  disableSunDay=(d:Date):boolean=>{
     const day = d.getDay()
     return day !== 0 // && day !== 6 // Uncomment if saturday is disabled too
   }
